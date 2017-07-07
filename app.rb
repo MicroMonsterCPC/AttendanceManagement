95183b5dfbda9051ce7ebfbeee220d30e0239f66
JpnLavender <irimamekun@gmail.com>
2017-07-08 07:15:37 +0900

    Create Users CRUD

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
+require "sinatra"
+require "sinatra/reloader"
+require "json"
 require "./models"
+require "./controllers/users_controller"
 
+# HOME 
 get '/' do
   "HelloWorld"
 end
 
+# Attendance management
+post '/attendance-record' do
+  params =  JSON.parse(request.body.read)
+  user = User.find_by(uuid: params["uuid"])
+end
---
+++ controllers/users_controller.rb
@@ -0,0 +1,24 @@
+# index
+get '/users' do
+  "users"
+end
+
+# show
+get '/users/:id' do
+end
+
+# new
+post '/users/new' do
+end
+
+# update
+put '/users/:id' do
+end
+
+# edit
+put '/users/:id/edit' do
+end
+
+# delete
+delete '/users/:id' do
+end
