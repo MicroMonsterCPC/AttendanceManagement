407c01735c6b5a9a0eeec4447dac0b0985d599b6
JpnLavender <irimamekun@gmail.com>
2017-07-14 06:15:43 +0900

    App.rbのインデント修正とattendanceをRequire

--- app.rb
+++ app.rb
@@ -4,19 +4,20 @@ require "sinatra/reloader"
 require "json"
 require "./models"
 require "./controllers/users_controller"
+require "./controllers/attendances_controller"
 
 class Public < Sinatra::Base
   # HOME 
   get '/' do
     "HelloWorld"
   end
-  end
+end
 
 class Protect < Sinatra::Base
   use Rack::Auth::Basic, "Protected Area" do |username, password|
     username == 'foo' && password == 'bar'
   end
-    get "/" do
+  get "/" do
     "secret page!"
   end
   get "/admin" do
