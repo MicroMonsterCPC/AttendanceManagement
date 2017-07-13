dd8f5c4bc7191e1bca8443c544bcedb7fe5a083e
JpnLavender <irimamekun@gmail.com>
2017-07-14 04:22:26 +0900

    Basic認証に伴うUrlなどの変更

--- controllers/users_controller.rb
+++ controllers/users_controller.rb
@@ -5,7 +5,6 @@ class Public < Sinatra::Base
     haml :"users/index"
   end
 
-
   # show
   get '/users/:id/show' do
     @user = User.find_by(id: params[:id])
@@ -13,47 +12,52 @@ class Public < Sinatra::Base
   end
 end
 
-# new
-get '/users/new' do
-  @form_action, @submit_title = "/users/#{params[:id]}/create", "Create"
-  @user = User.new
-  haml :"users/new"
-end
-
-post '/users/create' do
-  user = User.new({name: params[:name], uuid: params[:uuid]})
-  if user.save
-    @mes = "Success: User Created"
-  else
-    @mes = "Failure: User Created"
+class Protect < Sinatra::Base
+  # new
+  get '/users/new' do
+    @form_action = "/protect/users/create"
+    @submit_title = "Create"
+    @user = User.new
+    haml :"users/new"
   end
-  haml :"assets/message"
-end
 
-# edit
-get '/users/:id/edit' do
-  @form_action, @submit_title = "/users/#{params[:id]}/update", "Update"
-  @user = User.find_by(id: params[:id])
-  haml :"users/new"
-end
-
-# update
-post '/users/:id/update' do
-  user = User.find_by(id: params[:id])
-  if user.update( name: params[:name], uuid: params[:uuid])
-    @mes = "Success: User Update"
-  else
-    @mes = "Failure: User Update"
+  post '/users/create' do
+    user = User.new({name: params[:name], idm: params[:idm]})
+    if user.save
+      p user
+      @mes = "Success: User Created"
+    else
+      @mes = "Failure: User Created"
+    end
+    haml :"assets/message"
   end
-  haml :"assets/message"
-end
 
-# delete
-post '/users/:id/delete' do
-  if User.find_by(id: params[:id]).destory
-    @mes = "Success: User Delete"
-  else
-    @mes = "Failure: User Delete"
+  # edit
+  get '/users/:id/edit' do
+    @form_action= "/protect/users/#{params[:id]}/update"
+    @submit_title = "Update"
+    @user = User.find_by(id: params[:id])
+    haml :"users/new"
   end
-  haml :"assets/message"
+
+  # update
+  post '/users/:id/update' do
+    user = User.find_by(id: params[:id])
+    if user.update( name: params[:name], idm: params[:idm])
+      @mes = "Success: User Update"
+    else
+      @mes = "Failure: User Update"
+    end
+    haml :"assets/message"
+  end
+
+  # delete
+  get '/users/:id/delete' do
+    if User.find_by(id: params[:id]).destory
+      @mes = "Success: User Delete"
+    else
+      @mes = "Failure: User Delete"
+    end
+    haml :"assets/message"
+  end
 end
