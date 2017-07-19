b91c7c94890282c5ae90eec8bb24fcbac1dfc4df
JpnLavender <irimamekun@gmail.com>
2017-07-19 12:13:11 +0900

    attendances/createのif地帯を短縮化

--- controllers/attendances_controller.rb
+++ controllers/attendances_controller.rb
@@ -12,7 +12,7 @@ class Public < Sinatra::Base
   post '/attendance/user' do
     params = JSON.parse(request.body.read)
     if user = User.find_by(idm: params["idm"])
-      else
+    else
     end
   end
 end
@@ -39,36 +39,26 @@ class Protect < Sinatra::Base
   # 出席の登録
   post '/attendances/create' do
     params =  JSON.parse(request.body.read)
-    user = User.find_by(idm: params["idm"])
-    binding.pry
-    status = judge(user)
-    attendance = Attendance.new(
-      user: user,
-      record_time: Time.parse(params["datetime"]),
-      status: status
-    )
-    if Attendance.where(user: user, record_time: Date.today..Date.tomorrow, status: 1).exists?
-      {stauts: "already"}.to_json
-    elsif user.attendances.last && (user.attendances.last.record_time + 1.minute) > Time.now
-      {status: "early"}.to_json
-    else
-      p attendance if settings.development?
-      if attendance.save
-        {stauts: attendance.status}.to_json
-      else
-        {stauts: "error"}.to_json
-      end
+    users = User.where(idm: params["idm"])
+    users.each do |user|
+      status = judge(user)
+      attendance = Attendance.new( user: user, record_time: Time.parse(params["datetime"]), status: status)
+      (return {status: "already"}.to_json) if Attendance.where(user: user, record_time: Date.today..Date.tomorrow, status: 1).exists?
+      (return {status: "early"}.to_json)   if user.attendances.last && (user.attendances.last.record_time + 1.minute) > Time.now
+      attendance.save ? ( return {status: attendance.status}.to_json) : (return {status: "Error"})
     end
+    {status: "nouser"}.to_json
   end
 
   private
 
   def judge(user)
     # 0 => enter, 1 => left
+      binding.pry
     unless user.attendances.last
       return 0
     else
-      if Date.today > user.attendances.last.record_time
+      if Date.today.to_time > user.attendances.last.record_time
         return 0
       else
         case user.attendances.last.status
