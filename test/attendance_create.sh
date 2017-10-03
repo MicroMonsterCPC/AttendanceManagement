8f7974d52b38255c3ca69d4e02dc2423f3e6f4be
Maisu <mystasly48@gmail.com>
2017-10-03 19:44:32 +0900

    Fixed: datetime format changed %I to %H
    datetimeの書式を12時間表記から24時間表記に修正

--- test/attendance_create.sh
+++ test/attendance_create.sh
@@ -2,4 +2,4 @@ curl -X POST \
     http://localhost:4567/protect/attendances/create \
     -H 'Content-Type: application/json' \
     -u foo:bar \
-    -d '{"idm": "0101031299182322", "datetime": "'"$(date +"%Y/%m/%d %I:%M:%S") "'" }'
+    -d '{"idm": "0101031299182322", "datetime": "'"$(date +"%Y/%m/%d %H:%M:%S") "'" }'
